7c60b8cbc42f3f91de19adae4fbe0e3064eeb6a9
Mack Hendricks <mack@dopensource.com>
2021-04-22 10:40:32 -0400

    Update internal.tf

--- internal.tf
+++ internal.tf
@@ -1,1 +1,18 @@
+# Documentation
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
 
+resource "google_compute_network" "vpc_prod" {
+  name = "vpc-prod"
+  delete_default_routes_on_create = true
+}
+
+resource "google_compute_network" "vpc_uat" {
+  name = "vpc-uat"
+  delete_default_routes_on_create = true
+}
+
+resource "google_compute_network" "vpc_dev" {
+  name = "vpc-dev"
+  # Will prevent 
+  #delete_default_routes_on_create = true
+}
