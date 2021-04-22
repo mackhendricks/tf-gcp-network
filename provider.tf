b2d414943c611693fddb7560ba4d1510fe031dfa
Mack Hendricks <mack@dopensource.com>
2021-04-22 10:41:14 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 
 // Configure the Google Cloud Provider
 
+# Documentation: https://console.cloud.google.com/iam-admin/serviceaccounts
+
 provider "google" {
     project = "tf-gcp-example"
     region = "us-central"
