6702f702c84affda9d0520852e24480082af497f
Mack Hendricks <mack@dopensource.com>
2021-04-22 11:04:39 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,10 @@
 // Configure the Google Cloud Provider
 
 # Documentation: https://console.cloud.google.com/iam-admin/serviceaccounts
+# Add the contents of the JSON file to a TFE/Cloud Environment Variable.  
+# GOOGLE_CREDENTIALS = <Contents of JSON File>
+# The contents can not include any newline characters
+# Remove then in vi by doing this: %s;\n; ;g
 
 provider "google" {
     project = "tf-gcp-example"
